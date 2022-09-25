721cf03fd5156744c5f1964bfade08be38658e14
Bruno Pavlović <business.broots@gmail.com>
2022-09-25 21:22:09 +0200

    chapter 4 js-learning

---
+++ chapter_4/arguments_parameters.js
@@ -0,0 +1,8 @@
+// arguments & parameters
+
+const speak = function(name = 'luigi', time = 'night'){
+    console.log(`good ${time} ${name}`);
+};
+
+speak();
+speak('shaun');
---
+++ chapter_4/functions.js
@@ -0,0 +1,25 @@
+// function expression
+const speak = function(){
+    console.log('good day');
+};
+
+// greet();
+// greet();
+// greet();
+
+speak();
+speak();
+speak();
+
+// function declaration
+function greet(){
+    console.log('hello there');
+}
+
+
+// Arrow ES6 modern function
+
+const greetArrow = () => {
+    console.log('hello there');
+    
+}
