a2b551a0afc484b3ea3412bdcee5f94ec8da56ff
Bruno Pavlović <business.broots@gmail.com>
2022-10-04 21:32:38 +0200

    Further learning

--- udemy_tutorial/udemy.js
+++ udemy_tutorial/udemy.js
@@ -256,4 +256,33 @@ if(numNeighbours === 1) {
     console.log('No borders!');
 }*/
 
+// logical operators
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if(shouldDrive) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+const isTired = true; // C
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
+
+
+
 
